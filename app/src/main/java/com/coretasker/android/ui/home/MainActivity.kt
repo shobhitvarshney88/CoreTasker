5c6bed0309e20ad35741f4e309bb301add5442d9
shobhitvarshney88 <shobhitvarshney88@gmail.com>
2025-04-10 23:50:39 +0530

    added google sign in

--- app/src/main/java/com/coretasker/android/ui/home/MainActivity.kt
+++ app/src/main/java/com/coretasker/android/ui/home/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.coretasker.android.ui.home
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.coretasker.android.ui.theme.CoreTaskerTheme
+import com.coretasker.android.utils.GoogleSignInUtils
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +40,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
+        GoogleSignInUtils.doGoogleSignIn(context = context,scope,null, login = {
+            Toast.makeText(context,"Login Successfully", Toast.LENGTH_SHORT).show()
+        })
+    }
+
+
+    Button(
+        onClick = {
+            GoogleSignInUtils.doGoogleSignIn(context = context,scope,launcher, login = {
+                Toast.makeText(context,"Login Successfully", Toast.LENGTH_SHORT).show()
+            })
+        }
+    ) { }
+
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/coretasker/android/utils/GoogleSignInUtils.kt
@@ -0,0 +1,73 @@
+package com.coretasker.android.utils
+
+import android.R
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.result.ActivityResult
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.credentials.exceptions.NoCredentialException
+import com.google.android.gms.common.api.GoogleApi
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.Firebase
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class GoogleSignInUtils {
+    companion object{
+        fun doGoogleSignIn(context: Context,scope: CoroutineScope,launcher: ManagedActivityResultLauncher<Intent, ActivityResult>?,login:()->Unit){
+            val credentialManager = CredentialManager.create(context)
+
+            val googleIdOption = GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(true)
+                .setServerClientId(/*baseContext.getString(R.string.default_web_client_id)*/"1074384944818-kqfiqv3rfjfutj0kvhn5pod1m7bquqqs.apps.googleusercontent.com")
+                .build()
+
+            val request = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build()
+            scope.launch {
+                try {
+                    val result = credentialManager.getCredential(context,request)
+                    when(result.credential){
+                        is CustomCredential->{
+                            if(result.credential.type== GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL){
+                                val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(result.credential.data)
+                                val googleTokenId = googleIdTokenCredential.idToken
+                                val authCredential = GoogleAuthProvider.getCredential(googleTokenId,null)
+                                val user = Firebase.auth.signInWithCredential(authCredential).await().user
+                                user?.let {
+                                    if(it.isAnonymous.not()){
+                                        login.invoke()
+                                    }
+                                }
+                            }
+                        }
+                        else->{
+
+                        }
+                    }
+                }catch (e: NoCredentialException){
+                    launcher?.launch(getIntent())
+                }catch (e: GetCredentialException){
+                    e.printStackTrace()
+                }
+            }
+
+        }
+
+        fun getIntent(): Intent{
+            return Intent(Settings.ACTION_ADD_ACCOUNT).apply {
+                putExtra(Settings.EXTRA_ACCOUNT_TYPES, arrayOf("com.google"))
+            }
+        }
+    }
+}
